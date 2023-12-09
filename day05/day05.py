ebf5d8516382836f6515cd73a66791df3ad1e1a6
Jonathan Franzi <jonoerik@fastmail.fm>
2023-12-09 19:53:41 +1100

    Complete day05 part2

--- day05/day05.py
+++ day05/day05.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 import re
+from typing import Self
 
 # (destination range start, source range start, range length)
 SingleMappingType = tuple[int, int, int]
@@ -55,4 +56,101 @@ def part1(input_data: InputType) -> ResultType:
 
 
 def part2(input_data: InputType) -> ResultType:
-    pass  #TODO
+    class Range:
+        """
+        A range of numbers [start, end).
+        For convenience, ranges may be of zero length (start == end), covering no numbers.
+        """
+        def __init__(self, start: int, end: int) -> None:
+            self.start: int = start
+            self.end: int = end
+
+        def shift(self, x: int) -> None:
+            self.start += x
+            self.end += x
+
+        def __bool__(self):
+            return self.start != self.end
+
+        def intersection(self, other: Self) -> Self:
+            if other.start < self.start:
+                if other.end > self.start:
+                    return Range(self.start, min(self.end, other.end))
+                else:
+                    return Range(0, 0)
+            elif self.start <= other.start < self.end:
+                return Range(other.start, min(self.end, other.end))
+            else:
+                return Range(0, 0)
+
+        def sub(self, other: Self) -> list[Self]:
+            """
+            'Subtract'.
+            Remove all points in other from this range, and return the resulting ranges (0, 1, or 2).
+            """
+            if not self.intersection(other):
+                return [Range(self.start, self.end)]
+            elif other.start <= self.start <= self.end <= other.end:
+                return []
+            elif other.start <= self.start < other.end < self.end:
+                return [Range(other.end, self.end)]
+            elif self.start < other.start < self.end <= other.end:
+                return [Range(self.start, other.start - 1)]
+            elif self.start < other.start <= other.end < self.end:
+                return [Range(self.start, other.start - 1), Range(other.end, self.end)]
+            else:
+                assert False
+
+    class RangeList:
+        """
+        RangeLists may contain repeated ranges, overlapping ranges, and zero-length ranges.
+        These shouldn't affect the result, and getting rid of them is a lot of extra work.
+        """
+        def __init__(self, rl: list[Range]) -> None:
+            self.ranges: list[Range] = rl
+
+        def intersections(self, other: Range) -> Self:
+            result = RangeList([])
+            for r in self.ranges:
+                intersection = r.intersection(other)
+                if intersection:
+                    result.add(intersection)
+            return result
+
+        def remove(self, other: Range) -> None:
+            new_ranges: list[Range] = []
+            for r in self.ranges:
+                new_ranges += r.sub(other)
+            self.ranges = new_ranges
+
+        def add(self, other: Range) -> None:
+            self.ranges.append(other)
+
+        def min(self) -> int:
+            return min([r.start for r in self.ranges if r])
+
+    seeds = [Range(start, start + length) for start, length in zip(input_data[0][0::2], input_data[0][1::2])]
+
+    def apply_map(rl: RangeList, mapping: MapType) -> RangeList:
+        """
+        Note: this destructively modifies parameter rl.
+        """
+        new_rl = RangeList([])
+        for mapping_line in mapping[2]:
+            mapping_line_range = Range(mapping_line[1], mapping_line[1] + mapping_line[2])
+            intersections = rl.intersections(mapping_line_range)
+            for intersection in intersections.ranges:
+                rl.remove(intersection)
+                intersection.shift(mapping_line[0] - mapping_line[1])
+                new_rl.add(intersection)
+        for remainder in rl.ranges:
+            new_rl.add(remainder)
+        return new_rl
+
+    def seeds_to_location(r: Range) -> RangeList:
+        current = RangeList([r])
+        for mapping in input_data[1]:
+            current = apply_map(current, mapping)
+        return current
+
+    return min([locations.min() for locations in map(seeds_to_location, seeds)])
