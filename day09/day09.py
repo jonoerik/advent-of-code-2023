6f867de73b88bfadcdf65a0e99d308fb06b7db0d
Jonathan Franzi <jonoerik@fastmail.fm>
2023-12-09 20:21:15 +1100

    Complete day09 part2

--- day09/day09.py
+++ day09/day09.py
@@ -23,4 +23,12 @@ def part1(input_data: InputType) -> ResultType:
 
 
 def part2(input_data: InputType) -> ResultType:
-    pass  #TODO
+    def prev_num(l: list[int]) -> int:
+        sequence_deltas: list[list[int]] = [l]
+        while not all([x == 0 for x in sequence_deltas[-1]]):
+            sequence_deltas.append([b - a for a, b in itertools.pairwise(sequence_deltas[-1])])
+        # Result for this one takes the left side of sequence_deltas, and alternates between adding and subtracting
+        # values.
+        return sum([x if i % 2 == 0 else -x for i, x in enumerate([sd[0] for sd in sequence_deltas])])
+
+    return sum(map(prev_num, input_data))
