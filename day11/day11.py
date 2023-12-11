eeb9e49ce5769594999acc90918700ae671c8cde
Jonathan Franzi <jonoerik@fastmail.fm>
2023-12-11 23:12:49 +1100

    Complete day11 part1

---
+++ day11/day11.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+import itertools
+from pathlib import Path
+
+# True for each galaxy, false otherwise.
+InputType = list[list[bool]]
+ResultType = int
+
+
+def load(input_path: Path) -> InputType:
+    with open(input_path) as f:
+        return [[char == "#" for char in line.strip()] for line in f.readlines()]
+
+
+def part1(input_data: InputType) -> ResultType:
+    # Expand space.
+    empty_rows = reversed([row for row, line in enumerate(input_data) if not any(line)])
+    empty_cols = reversed([col for col in range(len(input_data[0])) if not any([line[col] for line in input_data])])
+    # Index lists are reversed, so early inserts don't alter the position of later inserts.
+    for empty_row in empty_rows:
+        input_data.insert(empty_row, [False for _ in range(len(input_data[0]))])
+    for empty_col in empty_cols:
+        for row in input_data:
+            row.insert(empty_col, False)
+
+    # Find galaxies, as a list of (row, col).
+    galaxies: list[tuple[int, int]] = \
+        [(row, col) for row, line in enumerate(input_data) for col, cell in enumerate(line) if cell]
+
+    return sum([abs(a[0] - b[0]) + abs(a[1] - b[1]) for a, b in itertools.combinations(galaxies, 2)])
+
+
+def part2(input_data: InputType) -> ResultType:
+    pass  #TODO
