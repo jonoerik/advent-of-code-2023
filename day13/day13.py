d02c71734eff8b7e4b5c0aa926b6633591c605f7
Jonathan Franzi <jonoerik@fastmail.fm>
2023-12-14 22:49:16 +1100

    Complete day13 part2

--- day13/day13.py
+++ day13/day13.py
@@ -34,23 +34,24 @@ def part1(input_data: InputType) -> ResultType:
             if is_v_reflection(image, i):
                 yield i
 
-    def is_h_reflection(image: SingleImageType, i: int) -> bool:
-        """Return True iff a line of reflection exists in image between rows i-1 and i."""
-        for col in range(0, len(image[0])):
-            a = [row[col] for row in image][i-1::-1]
-            b = [row[col] for row in image][i:]
-            if a[:len(b)] != b[:len(a)]:
-                return False
-        return True
-
-    def h_reflections(image: SingleImageType) -> Iterator[int]:
-        """Generator function returning the indexes of horizontal lines of reflection."""
-        for i in range(1, len(image)):
-            if is_h_reflection(image, i):
-                yield i
-
-    return sum([sum(v_reflections(image)) + 100 * sum(h_reflections(image)) for image in input_data])
+    # Use zip(*image) to transpose image, swapping rows for columns.
+    return sum([sum(v_reflections(image)) + 100 * sum(v_reflections([list(row) for row in zip(*image)]))
+                for image in input_data])
 
 
 def part2(input_data: InputType) -> ResultType:
-    pass  #TODO
+    def v_reflection_errors(image: SingleImageType, i: int) -> int:
+        """Return the number of characters that don't match in a reflection in image between columns i-1 and i."""
+        return sum([sum([1 if x != y else 0 for x, y in zip(a, b)])
+                    for a, b in [(line[i-1::-1], line[i:])for line in image]])
+
+    def v_smudged_reflection(image: SingleImageType) -> int:
+        """Return the index of a smudged vertical reflection in image, or 0 if none found."""
+        for i in range(1, len(image[0])):
+            if v_reflection_errors(image, i) == 1:
+                return i
+        return 0
+
+    # Use zip(*image) to transpose image, swapping rows for columns.
+    return sum([v_smudged_reflection(image) or 100 * v_smudged_reflection([list(row) for row in zip(*image)])
+                for image in input_data])
